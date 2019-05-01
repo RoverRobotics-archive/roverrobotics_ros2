85cf1e075411316adc77be53a3715bcb3b84bce5
Dan Rose <dan@digilabs.io>
2019-05-01 15:54:59 -0500

    Fix speed scaling

--- openrover_demo/launch/demo.launch.py
+++ openrover_demo/launch/demo.launch.py
@@ -6,11 +6,12 @@ from launch.events import Shutdown
 from launch_ros.actions import Node
 
 import sys
+
 print(sys.path)
 
+
 def generate_launch_description():
-    # want to use ExecuteComposableNodeProcess, but it's not in Crystal
-    # IncludeLaunchDescription doesn't seem to allow getting a launch file from another package
+    # todo: when ExecutableComposeNodeProcess is launched, use it.
     nodes = [
         Node(
             package='keystroke', node_executable='keystroke_listen', output='screen', node_name='keystroke_listen2',
@@ -18,7 +19,7 @@ def generate_launch_description():
         Node(
             package='keystroke', node_executable='keystroke_arrows_to_twist', output='screen',
             node_name='arrows_to_twist2',
-            parameters=[{'publish_period': 0.1, 'linear_scale': 1.0, 'angular_scale': 0.2}]),
+            parameters=[{'publish_period': 0.1, 'linear_scale': 0.1, 'angular_scale': 0.2}]),
         Node(
             package='openrover_core', node_executable='openrover', output='screen',
             arguments=[])
