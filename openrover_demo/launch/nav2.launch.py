7e9207ce00f623bba65df7afb09e1559a3e01786
Dan Rose <dan@digilabs.io>
2019-06-10 17:15:47 -0500

    update for lifecycle nav2

--- openrover_demo/launch/nav2.launch.py
+++ openrover_demo/launch/nav2.launch.py
@@ -13,60 +13,49 @@
 # limitations under the License.
 
 import os
-import time
 
 import launch.actions
 import launch_ros.actions
-from ament_index_python.packages import get_package_prefix, get_package_share_directory
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 
 
 # ros2 launch openrover_demo nav2.launch.py map:=install/share/openrover_demo/maps/map.yaml
 def generate_launch_description():
-    map_yaml_file = launch.substitutions.LaunchConfiguration('map')
-    use_sim_time = launch.substitutions.LaunchConfiguration('use_sim_time', default='false')
     params_file = launch.substitutions.LaunchConfiguration('params', default=os.path.join(
         get_package_share_directory('openrover_demo'), 'config', 'nav2.yaml'))
 
-    bt_navigator_install_path = get_package_prefix('nav2_bt_navigator')
-    bt_navigator_xml = os.path.join(bt_navigator_install_path,
-                                    'behavior_trees',
-                                    'navigate_w_recovery_retry.xml')  # TODO(mkhansen): change to an input parameter
-
     return LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'map', description='Full path to map file to load'),
-        launch.actions.DeclareLaunchArgument(
-            'use_sim_time', default_value='false', description='Use simulation (Gazebo) clock if true'),
-
         launch_ros.actions.Node(
-            package='nav2_map_server',
-            node_executable='map_server',
-            node_name='map_server',
-            output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, {'yaml_filename': map_yaml_file}]),
-        # lots of problems in the launch file related to spinning up too much at the same time.
-        # this ugly hack spaces things out to prevent crashes.
-        launch.actions.OpaqueFunction(function=(lambda *args: time.sleep(1))),
-        launch_ros.actions.Node(
-            package='nav2_world_model',
-            node_executable='world_model',
+            node_name='lifecycle_manager',
+            package='nav2_lifecycle_manager',
+            node_executable='lifecycle_manager',
             output='screen',
             parameters=[params_file]
         ),
-        launch_ros.actions.Node(
+        launch_ros.actions.LifecycleNode(
+            node_name='map_server',
+            package='nav2_map_server',
+            node_executable='map_server',
+            output='screen',
+            parameters=[params_file]
+        ),
+        launch_ros.actions.LifecycleNode(
+            node_name='amcl',
             package='nav2_amcl',
             node_executable='amcl',
             output='screen',
             parameters=[params_file],
         ),
-        launch_ros.actions.Node(
-            package='dwb_controller',
-            node_executable='dwb_controller',
+        launch_ros.actions.LifecycleNode(
+            node_name='world_model',
+            package='nav2_world_model',
+            node_executable='world_model',
             output='screen',
-            parameters=[params_file]),
-        launch.actions.OpaqueFunction(function=(lambda *args: time.sleep(1))),
-        launch_ros.actions.Node(
+            parameters=[params_file]
+        ),
+        launch_ros.actions.LifecycleNode(
+            node_name='navfn_planner',
             package='nav2_navfn_planner',
             node_executable='navfn_planner',
             output='screen',
@@ -76,13 +65,18 @@ def generate_launch_description():
             node_executable='motion_primitives_node',
             output='screen',
         ),
-        launch.actions.OpaqueFunction(function=(lambda *args: time.sleep(1))),
-        launch_ros.actions.Node(
-            package='nav2_bt_navigator',
+        launch_ros.actions.LifecycleNode(
             node_name='bt_navigator',
+            package='nav2_bt_navigator',
             node_executable='bt_navigator',
             output='screen',
-            parameters=[params_file, {
-                'bt_xml_filename': bt_navigator_xml}]
-        )
+            parameters=[params_file]
+        ),
+        launch_ros.actions.LifecycleNode(
+            node_name='dwb_controller',
+            package='dwb_controller',
+            node_executable='dwb_controller',
+            output='screen',
+            parameters=[params_file]
+        ),
     ])
