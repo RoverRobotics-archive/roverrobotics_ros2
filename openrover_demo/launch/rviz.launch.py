9f89f5e7ee8c7f25a163ecb9a299bbc5af339d4e
Dan Rose <dan@digilabs.io>
2019-06-27 18:07:19 -0500

    Expose RViz fixed frame as a command line variable for the launch file
    For SLAM, we want /odom to be the fixed frame but for point-click navigation, we want map.

--- openrover_demo/launch/rviz.launch.py
+++ openrover_demo/launch/rviz.launch.py
@@ -2,18 +2,23 @@ from pathlib import Path
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import SetEnvironmentVariable
+from launch.actions import SetEnvironmentVariable, DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    rviz_config = Path(get_package_share_directory('openrover_demo'), 'config', 'default.rviz')
+    rviz_config = Path(get_package_share_directory('openrover_demo'), 'config', 'default.rviz').resolve()
     assert rviz_config.is_file()
 
     return LaunchDescription([
+        DeclareLaunchArgument('frame', default_value='map', description='The fixed frame to be used in RViz'),
         SetEnvironmentVariable('RCUTILS_CONSOLE_STDOUT_LINE_BUFFERED', '1'),
         Node(
             package='rviz2', node_executable='rviz2', output='screen',
-            arguments=['--display-config', str(rviz_config)]
+            arguments=[
+                '--display-config', str(rviz_config),
+                '--fixed-frame', LaunchConfiguration(variable_name='frame')
+            ]
         )
     ])
