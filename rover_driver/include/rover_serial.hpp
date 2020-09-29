e2c3855379de8c60b393cb5f54e823540a4b9724
padiln <nicholas.padilla@npcomplete.io>
2020-09-29 11:39:30 -0500

    adopt terminos for serial communication

--- rover_driver/include/rover_serial.hpp
+++ rover_driver/include/rover_serial.hpp
@@ -4,7 +4,6 @@
 #include "rover_msgs/msg/raw_data.hpp"
 #include "rover_msgs/msg/raw_motor_command.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "serial/serial.h"  //< yes, this is a cpp header
 
 using namespace rover_msgs;
 using namespace std::chrono_literals;
@@ -13,6 +12,8 @@ using duration = std::chrono::nanoseconds;
 namespace rover
 {
 const uint8_t UART_START_PACKET = 253;
+const uint8_t SERIAL_OUT_PACKAGE_LENGTH = 7;
+const uint8_t SERIAL_IN_PACKAGE_LENGTH = 5;
 const unsigned long BAUDRATE = 57600;
 
 /// Responsible for managing the serial connection and communicating with the
@@ -24,6 +25,7 @@ public:
 
 protected:
   std::shared_ptr<const std::array<uint8_t, 3>> motor_efforts_u8;
+  std::string serial_port;
 
   /// Should send messages at this frequency, even if no data is requested.
   duration keepalive_period = 100ms;
@@ -43,11 +45,15 @@ protected:
   void keepalive_callback();
   void on_kill_motors();
   void on_motor_efforts(rover_msgs::msg::RawMotorCommand::SharedPtr msg);
+  bool open_serial();
+  unsigned int serial_read(std::vector<uint8_t> &inbuf, size_t size);
+  unsigned int serial_write(const uint8_t *data, size_t length);
+  unsigned int serial_buffer_availible();
   rclcpp::TimerBase::SharedPtr keepalive_timer;
   rclcpp::TimerBase::SharedPtr read_timer;
   rclcpp::TimerBase::SharedPtr kill_motors_timer;
 
-  std::shared_ptr<serial::Serial> serial_;
+  std::shared_ptr<int> serial_fd_;
 };
 
 class roverError : public std::runtime_error
